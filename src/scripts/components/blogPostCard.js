995c04d682e3a73fd2749d7aa883f5cc2da25c67
Codelicious <thibdema@student.arteveldehs.be>
2020-05-22 16:24:39 +0200

    Fixed zoom in effect on hover + blogposts

--- src/scripts/components/blogPostCard.js
+++ src/scripts/components/blogPostCard.js
@@ -2,7 +2,10 @@ export function makeBlogPostCard(data) {
   return `
     <li class="blogPostCard">
       <a href="#!/blogpost/detail/${data.id}" data-navigo>
-        <div  class="blogPostCard__thumbnail" style="background-image: url(${data.thumbnail})";></div>
+        
+        <div class="outer-div">
+          <div class="blogPostCard__thumbnail inner-div" style="background-image: url(${data.thumbnail})";></div>
+        </div>
         <div class="blogPostCard__text">
           <p class="blogPostCard__title">${data.title}</p>
           <p class="blogPostCard__subtitle">${data.subtitle}</p>
