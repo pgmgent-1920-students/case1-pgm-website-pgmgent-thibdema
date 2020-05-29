9f60513539bb4eb56a355ed7d8398350648be396
Codelicious <thibdema@student.arteveldehs.be>
2020-05-30 00:44:40 +0200

    3D threejs finetuned and fixed

--- src/scripts/pages/404.js
+++ src/scripts/pages/404.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three/build/three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
-import m1 from '../../3d-models/hammer.glb';
+import m1 from '../../3d-models/thonker.glb';
 
 export const main404 = async () => {
   const DOMELEMENT = document.querySelector('#element');
@@ -11,26 +11,27 @@ export const main404 = async () => {
 const lol = (DOM) => {
   // Renderer
   const renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setSize(document.body.clientWidth, window.innerHeight);
   renderer.outputEncoding = THREE.sRGBEncoding;
 
   // Canvas Element
   const canvas = renderer.domElement;
-  document.body.appendChild(canvas);
+  DOM.appendChild(canvas);
 
   // Scene
   const scene = new THREE.Scene();
-  scene.background = new THREE.Color(0x999999);
+  scene.background = new THREE.Color(0xffffff);
 
   // Camera
   const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight);
-  camera.position.set(30, 30, 30);
+  // camera.position.set(50, 0, 0);
+  camera.position.set(2, 0, 0);
   camera.lookAt(new THREE.Vector3(0, 0, 0));
 
   // Controls
 
   // Lights
-  const ambientLight = new THREE.AmbientLight(0x666666);
+  const ambientLight = new THREE.AmbientLight(0xffffff);
   scene.add(ambientLight);
 
   console.log(m1);
@@ -60,7 +61,6 @@ const lol = (DOM) => {
 
     // Model
     if (model) {
-      model.rotation.x += 0.01;
       model.rotation.y += 0.02;
     }
     
